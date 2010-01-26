03824fee6e9676d483d4d02ae2359796b6a4c48f
Andrew Williams <andy@tensixtyone.com>
2010-01-26 17:26:48 +0000

    Fixed console command init

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ license='GPLv2',
 py_modules = [ 'tvorganise'],
 entry_points = {
     'console_scripts':[
-        'tvorganise = tvorganise:TvOrganise.main'
+        'tvorganise = tvorganise:TvOrganiser.main'
     ]
 },
 
